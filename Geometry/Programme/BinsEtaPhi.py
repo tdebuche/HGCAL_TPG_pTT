449534556eaf5b550222f7f41192a1f0db59358e
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-05-13 13:46:13 +0200

    Update BinsEtaPhi.py

--- Geometry/Programme/BinsEtaPhi.py
+++ Geometry/Programme/BinsEtaPhi.py
@@ -4,34 +4,44 @@ import os
 
 #Entries
 etamin = 1.305
-etamax = etamin + np.pi *20/36
-
-phimin = 0
-phimax = 120
-
-nb_binphi = 24
-nb_bineta = 20
 
 #Create an array with the vertices of each bin. Bins are sorted by phi,eta
-def Bins(emin,nbeta,nbphi): 
+def Bins(etamin,phimin,nbeta,nbphi): 
     L = np.zeros((nbeta*nbphi,2,4))
-    phirun = 0
-    etarun = 0
-    poly = 0
+    bin = 0
     for i in range(nbphi):
         for j in range(nbeta):
-            L[poly] = np.array([[j * np.pi/36 + emin,(j+1) * np.pi/36 + emin,(j+1) * np.pi/36 + emin,j * np.pi/36 + emin],[i * np.pi/36,i * np.pi/36,(i+1) * np.pi/36,(i+1) * np.pi/36]])
-            poly+=1
+            L[poly] = np.array([[j * np.pi/36 + etamin,(j+1) * np.pi/36 + etamin,(j+1) * np.pi/36 + etamin,j * np.pi/36 + etamin],[i * np.pi/36  + phimin,i * np.pi/36 + phimin,(i+1) * np.pi/36 + phimin,(i+1) * np.pi/36 + phimin]])
+            bin+=1
     return(L)
 
 
 
-#record the file
-BIN = Bins(etamin,nb_bineta,nb_binphi)
+#record the files
 
+
+#without edges
+phimin = 0
+phimax = 120
+nb_binphi = 24
+nb_bineta = 20
+
+BIN = Bins(etamin,phimin,nb_bineta,nb_binphi)
 os.chdir("../Ressources")
 np.save('Binetaphi'+str(nb_bineta)+str(nb_binphi),BIN)
+os.chdir("../../ProgrammesRessources")
+np.save('Binetaphi'+str(nb_bineta)+str(nb_binphi),BIN)
 
+#with edges
+
+phimin = -15 * np.pi/180
+phimax = 125 * np.pi/18
+nb_binphi = 28
+nb_bineta = 20
+
+BIN = Bins(etamin,phimin,nb_bineta,nb_binphi)
+os.chdir("../Ressources")
+np.save('Binetaphi'+str(nb_bineta)+str(nb_binphi),BIN)
 os.chdir("../../ProgrammesRessources")
 np.save('Binetaphi'+str(nb_bineta)+str(nb_binphi),BIN)
 

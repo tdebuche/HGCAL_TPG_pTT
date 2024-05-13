5a8c5da31983914ae9ca06b941b4f15b08619ba6
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-05-13 14:40:11 +0200

    Update Plot_Geometry.py

--- Geometry/Programmes/Plot_Geometry.py
+++ Geometry/Programmes/Plot_Geometry.py
@@ -10,62 +10,61 @@ from functions import pointtopolygon
 from functions import binetaphitoXY
 from functions import binetaphiRADtoXY
 from functions import etaphicentre
-from functions import ModulestoSommets
-from functions import BintoBinSommets
-from functions import STCtoSTCSommets
+from functions import ModulestoVertices
+from functions import BintoBinVertices
+from functions import STCtoSTCVertices
 
 
 os.chdir("../Ressources")
 Z = np.load("Z.npy")
-G = np.load('Geometry.npy')
-UV = np.load('uv.npy')
-Binetaphi = np.load('Binetaphi.npy')
+G = np.load('ModulesGeometry.npy')
+UV = np.load('UVModules.npy')
+Binetaphi2024 = np.load('Binetaphi2024.npy') 
+Binetaphi2028 = np.load('Binetaphi2028.npy') 
 STCLD = np.load('STCLD.npy')
 STCHD = np.load('STCHD.npy')
 
-
+#In this programme, you can choose to plot (or not) modules, bins, STCs, and put the numbering of modules : u,v numbering, numbering of the layer, numbering of the board
 
 #Layer to draw
 Layer = 40
-
-
 zlay = Z[Layer-1]
 Modules = G[Layer-1]
-BinXY= binetaphitoXY(Binetaphi,zlay)
-Sommets = ModulestoSommets(Modules)
-BinSommets = BintoBinSommets(BinXY)
+BinXY2024= binetaphitoXY(Binetaphi2024,zlay)
+BinXY2028= binetaphitoXY(Binetaphi2028,zlay)
+ModuleVertices = ModulestoVertices(Modules)
+BinVertices = BintoBinVertices(BinXY)
 uv = UV[Layer-1]
 
-
+#for the numbering of input S1
 min_numberingmod = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 146, 198, 0, 52, 72, 95, 72, 95]
-
 min_numberingscint = [132, 109, 136, 109, 104, 122, 110, 132, 118, 250, 114, 114, 95, 95]
-
 IndminScint = [95,95,95,95,72,72,52,52,52,52,37,37,37,37]
 
 plt.figure(figsize = (12,8))
 
-
 #Modules
-for i in range(len(Sommets)):
-    plt.plot(Sommets[i][0] + [Sommets[i][0][0]],Sommets[i][1]+ [Sommets[i][1][0]], color = 'black')
+for i in range(len(ModuleVertices)):
+    plt.plot(ModuleVertices[i][0] + [ModuleVertices[i][0][0]],ModuleVertices[i][1]+ [ModuleVertices[i][1][0]], color = 'black')
     eta,phi = etaphicentre(Modules[i],zlay)
     x,y = etaphitoXY(eta,phi,zlay)
-    plt.annotate(str(i),(x - 60,y -10),size =  '8')
+    #plt.annotate(str(i),(x - 60,y -10),size =  '8')
 print(str(i))
+
+
 #Bins
 for i in range(len(BinXY)):
-   plt.plot(BinSommets[i][0] + [BinSommets[i][0][0]],BinSommets[i][1]+ [BinSommets[i][1][0]], color = 'red',linewidth = '0.5')
+   plt.plot(BinVertices[i][0] + [BinVertices[i][0][0]],BinVertices[i][1]+ [BinVertices[i][1][0]], color = 'red',linewidth = '0.5')
 
 #UV
-for i in range(len(Sommets)):
+for i in range(len(ModuleVertices)):
     eta,phi = etaphicentre(Modules[i],zlay)
     x,y = etaphitoXY(eta,phi,zlay)
     #plt.annotate('(' + str(uv[i][0]) +','+str(uv[i][1])+')',(x - 60,y -10),size =  '8')
 
 
 #Numbering
-for i in range(len(Sommets)):
+for i in range(len(ModuleVertices)):
     eta,phi = etaphicentre(Modules[i],zlay)
     x,y = etaphitoXY(eta,phi,zlay)
     if Layer > 33:
@@ -85,6 +84,7 @@ plt.xlabel('x (mm)')
 plt.ylabel('y (mm)')
 plt.show()
 
+#numbering the eta,phi bins
 """
 #marker phi
 for i in range(24):
@@ -106,14 +106,15 @@ for i in range(20):
     y = (BinXY[460+i][1][3] + BinXY[460+i][1][3])/2 -100
     plt.annotate('eta='+str(i),(x,y),size =  '8')"""
 
-"""
-os.chdir("../Ressources")
-plt.savefig('pTT of layer '+str(Layer)+'.png')"""
 
 #record  all layers
-
-os.chdir("../Ressources")
 """
+#choose the good directory
+#os.chdir("../Ressources/LayerswithbinswithSTCs")
+#os.chdir("../Ressources/LayerswithUV")  
+#os.chdir("../Ressources/Layerswithnumbering") 
+#os.chdir("../Ressources/Layerswithbins") 
+
 for k in range(0,34):
     if k <13:
         Layer = 2 *k+1
@@ -122,23 +123,26 @@ for k in range(0,34):
     zlay = Z[Layer-1]
     Modules = G[Layer-1]
     BinXY= binetaphitoXY(Binetaphi,zlay)
-    Sommets = ModulestoSommets(Modules)
-    BinSommets = BintoBinSommets(BinXY)
+    ModuleVertices = ModulestoVertices(Modules)
+    BinVertices = BintoBinVertices(BinXY)
     uv = UV[Layer-1]
     if Layer >33:
         STC = STCLD[Layer-34]
-        STCSommets = STCtoSTCSommets(STCLD[Layer-34])
+        STCVertices = STCtoSTCVertices(STCLD[Layer-34])
     if Layer <34 and Layer > 26:
         STC = STCHD[Layer-27]
-        STCSommets = STCtoSTCSommets(STCHD[Layer-27])
+        STCVertices = STCtoSTCVertices(STCHD[Layer-27])
     plt.figure(figsize = (12,8))
-    plt.title(label =  'pTT of layer '+str(Layer))
+    plt.title(label =  'Layer '+str(Layer))
     plt.xlabel('x (mm)')
     plt.ylabel('y (mm)')
-    for i in range(len(Sommets)):
-        plt.plot(Sommets[i][0] + [Sommets[i][0][0]],Sommets[i][1]+ [Sommets[i][1][0]], color = 'black')
+    for i in range(len(ModuleVertices)):
+        plt.plot(ModuleVertices[i][0] + [ModuleVertices[i][0][0]],ModuleVertices[i][1]+ [ModuleVertices[i][1][0]], color = 'black')
         eta,phi = etaphicentre(Modules[i],zlay)
         x,y = etaphitoXY(eta,phi,zlay)
+
+
+        #if numbering
         if Layer > 33:
             if i < IndminScint[Layer-34]:
                 N = min_numberingmod[Layer-14]
@@ -149,14 +153,23 @@ for k in range(0,34):
         else :
             N = min_numberingmod[Layer//2]
             #plt.annotate(str(N+i),(x - 60,y -10),size =  '8')
+
+            
+        #if uv
         #plt.annotate('(' + str(uv[i][0]) +','+str(uv[i][1])+')',(x - 60,y -10),size =  '8')
+
+
+    #if withbins
     #for i in range(len(BinXY)):
-        #plt.plot(BinSommets[i][0] + [BinSommets[i][0][0]],BinSommets[i][1]+ [BinSommets[i][1][0]], color = 'red',linewidth = '0.5')
+         #plt.plot(BinVertices[i][0] + [BinVertices[i][0][0]],BinVertices[i][1]+ [BinVertices[i][1][0]], color = 'red',linewidth = '0.5')
+
+
+    #if STCs
     if Layer > 26:
-        for i in range(len(STC)):
-            for j in range(len(STC[i])):
-                if i < len(STCSommets):
-                    if j< len(STCSommets[i]):
-                        stc = STCSommets[i][j]
-                        plt.plot(stc[0]+[stc[0][0]],stc[1]+[stc[1][0]],linewidth = 0.2,color  = 'blue') #STC
-    plt.savefig('Modules of layer '+str(Layer)+'.png')"""
+        for i in range(len(STCVertices)):
+            for j in range(len(STCVertices[i])):
+                stc = STCVertices[i][j]
+                #plt.plot(stc[0]+[stc[0][0]],stc[1]+[stc[1][0]],linewidth = 0.2,color  = 'blue') #STC
+
+                
+    plt.savefig('Layer '+str(Layer)+'.png')"""

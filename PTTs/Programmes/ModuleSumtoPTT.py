5623af34ebe88743dbdea33ad951734a07de880f
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-05-14 16:45:35 +0200

    Update ModuleSumtoPTT.py

--- PTTs/Programmes/ModuleSumtoPTT.py
+++ PTTs/Programmes/ModuleSumtoPTT.py
@@ -15,7 +15,7 @@ Values2028 = np.load('ValuesBins2028.npy')
                      
 
 N = 16 #energies divided by N (for the sharing)
-
+etamin = 1.305
 #########################Build PTTs : array(nb_modules,nb_PTTs,3) (module,PTTs)-->[phiBin,etaBin,ratio] ########################
 
 

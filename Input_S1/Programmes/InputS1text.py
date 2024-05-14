eed55f4325c7803c41fa5f55b5d15a3c6e25540b
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-05-14 07:05:56 +0200

    Update InputS1text.py

--- Input_S1/Programmes/InputS1text.py
+++ Input_S1/Programmes/InputS1text.py
@@ -1,36 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-from prelim import etaphitoXY
-from prelim import etaphiRADtoXY
-from prelim import XYtoetaphi
-from prelim import polygontopoints
-from prelim import pointtopolygon
-from prelim import binetaphitoXY
-from prelim import binetaphiRADtoXY
-from prelim import etaphicentre
-from prelim import ModulestoSommets
-from prelim import BintoBinSommets
-from prelim import STCtoSTCSommets
+from functions  import etaphitoXY
+from functions  import etaphiRADtoXY
+from functions  import XYtoetaphi
+from functions  import polygontopoints
+from functions  import pointtopolygon
+from functions  import binetaphitoXY
+from functions  import binetaphiRADtoXY
+from functions  import etaphicentre
+from functions import ModulestoVertices
+from functions import STCtoSTCVertices
 
 
-os.chdir("C:/Users/Thomas de L'Epinois/Desktop/StageCMS/Mapping/pTT/Ressources")
+os.chdir("../../ProgrammesRessources")
 Z = np.load("Z.npy")
-G = np.load('Geometry.npy')
-UV = np.load('uv.npy')
-Binetaphi = np.load('Binetaphi.npy')
+G = np.load('ModulesGeometry.npy')
+UV = np.load('UVModules.npy')
 STCLD = np.load('STCLD.npy')
 STCHD = np.load('STCHD.npy')
 
+
 min_numberingmod = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 146, 198, 0, 52, 72, 95, 72, 95]
-
 min_numberingscint = [132, 109, 136, 109, 104, 122, 110, 132, 118, 250, 114, 114, 95, 95]
-
 IndminScint = [95,95,95,95,72,72,52,52,52,52,37,37,37,37]
-
 Boards = [['0x64000000', 3, 34], ['0x64010000', 1, 36, 47], ['0x64020000', 33, 40, 41], ['0x64030000', 9, 39, 44], ['0x64040000', 7, 42, 43], ['0x64050000', 13, 38, 46], ['0x64060000', 17, 27], ['0x64070000', 25, 31], ['0x64080000', 23, 30], ['0x64090000', 15, 32], ['0x640A0000', 19, 29], ['0x640B0000', 21, 28], ['0x640C0000', 5, 35], ['0x640D0000', 11, 37, 45]]
-
-
 Boards_scintillators = [['0x64000000', 47], ['0x64010000',41], ['0x64020000',43], ['0x64030000', 37], ['0x64040000', 38], ['0x64050000', 35], ['0x64060000', 40], ['0x64070000', 39], ['0x64080000',42], ['0x64090000', 36], ['0x640A0000', 44], ['0x640B0000',45], ['0x640C0000', 46], ['0x640D0000', 34]]
 
 
@@ -78,7 +72,7 @@ def input():
     return tCEE,tCEH
 
 
-os.chdir("C:/Users/Thomas de L'Epinois/Desktop/StageCMS/Mapping/pTT/Ressources/Info_Boards")
+os.chdir("../Input_S1/Ressources")
 textCEE,textCEH = input()
 file = open('Input_CEE_pTT.txt', "w")
 file.write(textCEE)

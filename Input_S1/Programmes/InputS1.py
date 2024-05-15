3cc7abf397d2a699acf69de91b4c12f1055cb6c4
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-05-15 14:52:39 +0200

    Update InputS1.py

--- Input_S1/Programmes/InputS1.py
+++ Input_S1/Programmes/InputS1.py
@@ -1,19 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-from functions  import etaphitoXY
-from functions  import etaphiRADtoXY
-from functions  import XYtoetaphi
-from functions  import polygontopoints
-from functions  import pointtopolygon
-from functions  import binetaphitoXY
-from functions  import binetaphiRADtoXY
-from functions  import etaphicentre
-from functions import ModulestoVertices
-from functions import STCtoSTCVertices
+import functions
+import argparse
 
-
-os.chdir("../../ProgrammesRessources")
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path+"/../../ProgrammesRessources")
 Z = np.load("Z.npy")
 G = np.load('ModulesGeometry.npy')
 UV = np.load('UVModules.npy')
@@ -27,10 +19,15 @@ Boards = [['0x64000000', 3, 34], ['0x64010000', 1, 36, 47], ['0x64020000', 33, 4
 Boards_scintillators = [['0x64000000', 47], ['0x64010000',41], ['0x64020000',43], ['0x64030000', 37], ['0x64040000', 38], ['0x64050000', 35], ['0x64060000', 40], ['0x64070000', 39], ['0x64080000',42], ['0x64090000', 36], ['0x640A0000', 44], ['0x640B0000',45], ['0x640C0000', 46], ['0x640D0000', 34]]
 
 #Board to record
-Board = 13
+parser = argparse.ArgumentParser()
+parser.add_argument("Board", help="Board to display",type=int)
+parser.add_argument("--SizeX",default = 34, help="figure size",type =int)
+parser.add_argument("--SizeY",default = 16, help="figure size",type = int)
+args = parser.parse_args()
+Board = args.Board
+xsize,ysize = args.SizeX,args.SizeY
 
-os.chdir("../Input_S1/Ressources")
-plt.figure(figsize = (34,16))  #need to adjust the size of the picture
+plt.figure(figsize = (xsize,ysize)) 
 
 #Look at the SI modules layers
 res = 0
@@ -39,15 +36,15 @@ for lay in range(1,len(Boards[Board])):
     Layer = Boards[Board][lay]
     zlay = Z[Layer-1]
     Modules = G[Layer-1]
-    ModuleVertices = ModulestoVertices(Modules)
+    ModuleVertices = functions.ModulestoVertices(Modules)
     uv = UV[Layer-1]
     if Layer < 27:
         plt.annotate('LayerCEE '+str(Layer),(80+res,1650))
     else :
         plt.annotate('LayerCEH '+str(Layer),(80+res,1650))
     for i in range(len(ModuleVertices)):
-        eta,phi = etaphicentre(Modules[i],zlay)
-        x,y = etaphitoXY(eta,phi,zlay)
+        eta,phi = functions.etaphicentre(Modules[i],zlay)
+        x,y = functions.etaphitoXY(eta,phi,zlay)
         if Layer > 33:
             if i < IndminScint[Layer-34]:
                 N = min_numberingmod[Layer-14]
@@ -73,12 +70,12 @@ for lay in range(1,len(Boards[Board])):
 Layer = Boards_scintillators[Board][1]
 zlay = Z[Layer-1]
 Modules = G[Layer-1]
-ModuleVertices = ModulestoVertices(Modules)
+ModuleVertices = fnuctions.ModulestoVertices(Modules)
 uv = UV[Layer-1]
 plt.annotate('LayerCEH '+str(Layer),(80+res,1650))
 for i in range(len(ModuleVertices)):
-    eta,phi = etaphicentre(Modules[i],zlay)
-    x,y = etaphitoXY(eta,phi,zlay)
+    eta,phi = functions.etaphicentre(Modules[i],zlay)
+    x,y = functions.etaphitoXY(eta,phi,zlay)
     if i >= IndminScint[Layer-34]:
         plt.plot(np.array((ModuleVertices[i][0] + [ModuleVertices[i][0][0]]))*multiple   + res,np.array(ModuleVertices[i][1]+ [ModuleVertices[i][1][0]])*multiple , color = 'black')
         plt.plot(np.array((ModuleVertices[i][0] + [ModuleVertices[i][0][0]]))*multiple  + res,np.array(ModuleVertices[i][1]+ [ModuleVertices[i][1][0]])*multiple -2500, color = 'black')
@@ -91,5 +88,5 @@ plt.yticks([])
 plt.title('Input of Boards '+str(Board))
 plt.show()
 
-
+os.chdir(dir_path+"/../Ressources")
 plt.savefig('Input of Boards '+str(Board)+'.png')

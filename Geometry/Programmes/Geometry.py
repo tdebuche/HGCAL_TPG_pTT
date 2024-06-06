fc92ce81e0b4300024cf1339a63dcf8f91af8115
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-06 15:47:06 +0200

    Update Geometry.py

--- Geometry/Programmes/Geometry.py
+++ Geometry/Programmes/Geometry.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 
 
 def read_xml():
-    tree = ET.parse('../Ressources/geometry.xml')
+    tree = ET.parse('../Ressources/Geometry.xml')
     root = tree.getroot()
     Modules = [[] for layer in range(47)]
     layer = 0

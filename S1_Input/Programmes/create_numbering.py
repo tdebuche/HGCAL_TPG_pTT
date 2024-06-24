8eb05bdb856abff6d0a7cafc37ae2359b8de92d0
tdebuche <thomas.de-buchere-de-lepinois@polytechnique.edu>
2024-06-24 10:13:59 +0200

    update

--- S1_Input/Programmes/create_numbering.py
+++ S1_Input/Programmes/create_numbering.py
@@ -11,16 +11,6 @@ with open('ProgrammesRessources/STCs.json','r') as file:
 Boards = [['0x64000000', 3, 34], ['0x64010000', 1, 36, 47], ['0x64020000', 33, 40, 41], ['0x64030000', 9, 39, 44], ['0x64040000', 7, 42, 43], ['0x64050000', 13, 38, 46], ['0x64060000', 17, 27], ['0x64070000', 25, 31], ['0x64080000', 23, 30], ['0x64090000', 15, 32], ['0x640A0000', 19, 29], ['0x640B0000', 21, 28], ['0x640C0000', 5, 35], ['0x640D0000', 11, 37, 45]]
 Boards_scintillators = [['0x64000000', 47], ['0x64010000',41], ['0x64020000',43], ['0x64030000', 37], ['0x64040000', 38], ['0x64050000', 35], ['0x64060000', 40], ['0x64070000', 39], ['0x64080000',42], ['0x64090000', 36], ['0x640A0000', 44], ['0x640B0000',45], ['0x640C0000', 46], ['0x640D0000', 34]]
 
-def record_numbering():
-    CEE_numbering,CEH_numbering = create_module_numbering()
-    STC_numbering = create_STC_numbering()
-    with open('ProgrammesRessources/CEE_numbering.json', 'w') as file:
-        json.dump(CEE_numbering, file)
-    with open('ProgrammesRessources/CEH_numbering.json', 'w') as file:
-        json.dump(CEH_numbering, file)
-    with open('Python_Geometry/STC_numbering.json', 'w') as file:
-        json.dump(STC_numbering, file)
-
     
 def create_module_numbering():
   CEE_numbering = defaultdict()
@@ -59,9 +49,9 @@ def create_STC_numbering():
       Layer = Layers[lay]
       if Layer >26:
         for STC_idx in range(len(STCs[Layer-1])):
-          stc = STCs[Layer-1][module_idx]
-          if stc['type'] == 'silicon': stc = 0 
-          if stc['type'] == 'scintillator': stc = 1
+          stc = STCs[Layer-1][STC_idx]
+          if stc['type'] == 'silicon': stc_type = 0 
+          if stc['type'] == 'scintillator': stc_type = 1
           stc_type = stc['type']
           if (stc_type=='silicon' and lay!=len(Layers)-1) or (stc_type=='scintillator' and lay==len(Layers)-1):
             stc_u = stc['u']

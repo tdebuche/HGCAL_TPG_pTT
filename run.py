f044bfb0c5435e6070d2de13b38a83de8d61765a
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-24 10:21:43 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from S1_Input.Programmes.create_numbering import create_module_numbering
+from S1_Input.Programmes.create_numbering import record_numbering
 
 #create S1 numbering
 print(create_module_numbering())
+record_numbering()

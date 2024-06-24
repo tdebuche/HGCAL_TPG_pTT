55b3568d10ac57f22fcca6f348b6d9cad4ab024d
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-24 10:42:47 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
 from S1_Input.Programmes.create_numbering import create_module_numbering
-
+from pTTs.Programmes.pTTs_to_file import record_all_boards
 
 #create S1 numbering
 print(create_module_numbering())
 
+#record pTTs
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--Format",default = 'readable', help="textfile : readable, vhfile: vh")
+parser.add_argument("--STCs",default = 'yes', help="With (yes) or without STCs (no)")
+parser.add_argument("--Edges",default = 'no', help="With (yes) or without edges(no)")
+args = parser.parse_args()
+
+
+record_all_boards(args)

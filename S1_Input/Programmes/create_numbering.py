292e9ae83055d25fa66b342ef9a730b3ef2039fe
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-24 13:33:05 +0200

    Update create_numbering.py

--- S1_Input/Programmes/create_numbering.py
+++ S1_Input/Programmes/create_numbering.py
@@ -27,6 +27,7 @@ def create_module_numbering():
   CEE_numbering = defaultdict(list)
   CEH_numbering = defaultdict(list)
   CEE_count,CEH_count = 0,0
+  nb_modules_per_board = []
   for Board_number in range(14):
     Layers = Boards[Board_number][1:]
     Layers.append(Boards_scintillators[Board_number][1])
@@ -44,14 +45,16 @@ def create_module_numbering():
           if Layer > 26:
             CEH_numbering[(Layer,module_type,module_u,module_v)].append(CEH_count)
             CEH_count +=1
+    nb_modules_per_board.append({'Board':Board_number,'CEE_count':CEE_count,'CEH_count':CEH_count})
     CEE_count,CEH_count = 0,0
-  return(CEE_numbering,CEH_numbering)
+  return(CEE_numbering,CEH_numbering,nb_modules_per_board)
     
 
 
 def create_STC_numbering():
   STC_numbering = defaultdict(list)
-  module_count,STC_count = 0,0
+  channel_count,STC_count = 0,0
+  STC_channels_per_board = []
   for Board_number in range(14):
     Layers = Boards[Board_number][1:]
     Layers.append(Boards_scintillators[Board_number][1])
@@ -68,31 +71,34 @@ def create_STC_numbering():
             if STC_numbering[(Layer,stc_type,stc_u,stc_v,0)] == []:
               STC_numbering[(Layer,stc_type,stc_u,stc_v,0)].append(module_count)
               STC_numbering[(Layer,stc_type,stc_u,stc_v,0)].append(defaultdict(list))
-              module_count += 1
+              channel_count += 1
             if len(STC_numbering[(Layer,stc_type,stc_u,stc_v,0)]) < 6:
               STC_count = len(STC_numbering[(Layer,stc_type,stc_u,stc_v,0)][1])
               STC_numbering[(Layer,stc_type,stc_u,stc_v,0)][1][stc_idx].append(STC_count)
               
             if len(STC_numbering[(Layer,stc_type,stc_u,stc_v,0)]) == 6:
               if STC_numbering[(Layer,stc_type,stc_u,stc_v,1)] == []:
-                STC_numbering[(Layer,stc_type,stc_u,stc_v,1)].append(module_count)
+                STC_numbering[(Layer,stc_type,stc_u,stc_v,1)].append(channel_count)
                 STC_numbering[(Layer,stc_type,stc_u,stc_v,1)].append(defaultdict(list))
-                module_count += 1
+                channel_count += 1
               STC_count = len(STC_numbering[(Layer,stc_type,stc_u,stc_v,1)][1])
               STC_numbering[(Layer,stc_type,stc_u,stc_v,1)][1][stc_idx].append(STC_count)
-    module_count,STC_count = 0,0
-  return(STC_numbering)
+    STC_channels_per_board.append({'Board':Board_number,'STCs_count':channel_count})
+    channel_count,STC_count = 0,0
+  return(STC_numbering,STC_channels_per_board)
 
 
-CEE_numbering,CEH_numbering = create_module_numbering()
-STC_numbering = create_STC_numbering()
+CEE_numbering,CEH_numbering,nb_modules_per_board = create_module_numbering()
+STC_numbering,STC_channels_per_board = create_STC_numbering()
 
 
 
-def nb_inputs(args,Board):
-  nb_CEE_inputs,nb_CEH_inputs = 0,0
-  return(nb_CEE_inputs,nb_CEH_inputs)
-
+def nb_inputs(args,Board_number):
+  if args.Edges == "yes":
+    return(nb_modules_per_board[Board_number]['CEE_count'],nb_modules_per_board[Board_number]['CEH_count'])
+  if args.Edges == "no":
+    return(nb_modules_per_board[Board_number]['CEE_count'],STC_channels_per_board[Board_number]['STCs_count'])
+    
 def get_module_channel(Layer,type,module_u,module_v):
     if Layer < 27:
         return(CEE_numbering[(Layer,type,module_u,module_v)][0])

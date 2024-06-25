8b3a87b5151ae3276af024b4a9973535c93a33a8
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-25 11:38:27 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,17 +1,61 @@
 import argparse
-from pTTs.Programmes.pTTs_to_file import record_all_boards
-from S1_Input.Programmes.record_text import record_input
+from pTTs.Programs.pTTs_to_file import record_all_boards
+from S1_Input.Programs.record_text import record_input
+from Geometry.Programs.plot import plot_layer
+from Geometry.Programs.plot import record_all_layers
 
 #record pTTs
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--Format",default = 'readable', help="textfile : readable, vhfile: vh")
+
+#Scenario
 parser.add_argument("--STCs",default = 'yes', help="With (yes) or without STCs (no)")
 parser.add_argument("--Edges",default = 'no', help="With (yes) or without edges(no)")
+
+#Build and record pTTs
+parser.add_argument("--pTTs",default = 'no', help="build and record the pTTs")
+parser.add_argument("--Format",default = 'readable', help="textfile : readable, vhfile: vh")
+
+#Plot a layer 
+parser.add_argument("--Plot",default = 'no', help="plot a layer")
+parser.add_argument("--Layer", default = 1 ,help="Layer to display",type=int)
+parser.add_argument("--UV",default = 'no', help="With or without UV")
+parser.add_argument("--irot",default = 'no', help="With or without rot")
+parser.add_argument("--Bins",default = 'no', help="With or without bins")
+#Record plots 
+parser.add_argument("--Record_plots",default = 'no', help="record all layers")
+
+#S1_Input
+parser.add_argument("--S1_Input",default = 'no', help="run and record the S1 numbering")
+
+#S1-S2_Mapping
+parser.add_argument("--S1S2_Mapping",default = 'no', help="run and record the S1-S2_Mapping")
+
 args = parser.parse_args()
 
 
-#record_all_boards(args)
-record_all_boards(args)
-#create S1 numbering
-record_input(args)
+
+#Build and record files to build pTTs
+if args.pTTs == "yes":
+  record_all_boards(args)
+
+
+#create and record the S1 numbering
+if args.S1_Input == "yes":
+  record_input(args)
+
+#plot Layer
+if args.Plot == "yes":
+  plot_layer(args)
+
+#Record plots of all layer
+if args.Record_plots == "yes":
+  record_all_layers(args)
+
+
+
+
+
+
+
+

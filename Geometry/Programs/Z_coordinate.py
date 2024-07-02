645a334bc56126b299a4ce41f14679a5f0546d8e
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-07-02 16:21:53 +0200

    Update Z_coordinate.py

--- Geometry/Programs/Z_coordinate.py
+++ Geometry/Programs/Z_coordinate.py
@@ -1,6 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import os
+import json
 
 ############################# Return a  list(47) with z for each layers ############################################################
 
@@ -9,32 +7,24 @@ ZlayerCEHfine = [3665.96,3687.51,3729.01,3750.56,3792.01,3813.61,3855.11,3876.66
 ZlayerCEHcoarse = [4378.71,4400.26,4460.96,4482.51,4543.21,4564.76,4625.46,4647.01,4707.71,4729.26,4789.96,4811.51,4872.21,4893.76,4954.46,4976.01,5036.71,5058.26,5118.96,5140.51]
 
 
-def Zmiddle(L1,L2,L3):
+def create_Z_coordinates(L1,L2,L3):
     Z = []
+    layer = 1
     for i in range(len(L1)-1):
-        Z.append((L1[i]+L1[i+1])/2)
-        Z.append((L1[i]+L1[i+1])/2)
+        z = (L1[i]+L1[i+1])/2)
+        Z.append({'Layer' : layer, 'Z_coordinate' : z})
+        layer += 1
+        Z.append({'Layer' : layer, 'Z_coordinate' : z})
+        layer += 1
     for i in range(len(L2)//2):
-        Z.append((L2[2*i]+L2[2*i+1])/2)
+        z = (L2[2*i]+L2[2*i+1])/2
+        Z.append({'Layer' : layer, 'Z_coordinate' : z})
+        layer += 1
     for i in range(len(L3)//2):
-        Z.append((L3[2*i]+L3[2*i+1])/2)
+        z = (L3[2*i]+L3[2*i+1])/2
+        Z.append({'Layer' : layer, 'Z_coordinate' : z})
+        layer += 1
     return(Z)
 
-Z  = Zmiddle(ZlayerCEE,ZlayerCEHfine,ZlayerCEHcoarse)
-
-
-plt.plot(ZlayerCEE,[0 for i in ZlayerCEE],'o',color = 'red')
-plt.plot(ZlayerCEHfine,[0 for i in ZlayerCEHfine],'o',color = 'green')
-plt.plot(ZlayerCEHcoarse,[0 for i in ZlayerCEHcoarse],'o',color = 'blue')
-
-plt.plot(Z,[0 for i in Z],'o', color = 'black')
-
-
-
-
-plt.show()
-os.chdir("../Ressources")
-np.save('Z.npy',Z)
-
-os.chdir("../../Ressources")
-np.save('Z.npy',Z)
+with open('src/Z_coordinates.json', 'w') as recordfile:
+    json.dump(create_Z_coordinates(ZlayerCEE,ZlayerCEHfine,ZlayerCEHcoarse), recordfile)

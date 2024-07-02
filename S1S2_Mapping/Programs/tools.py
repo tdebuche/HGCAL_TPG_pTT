8787c6422598d88f53821865d491f738788577b5
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-07-02 14:22:43 +0200

    Update tools.py

--- S1S2_Mapping/Programs/tools.py
+++ S1S2_Mapping/Programs/tools.py
@@ -1,7 +1,13 @@
 
 Endcap = 0
 Subsector = 0
-
+def get_pTT_numbers(pTT):
+    S1Board = int(pTT[4:6],16) & 0x3F
+    phi = int(pTT,16) & 0x1F
+    eta = (int(pTT,16) & 0x3E0) //(16 * 2)
+    CEECEH = (int(pTT,16) & 0x400) //(16*16*4)
+    Sector = (int(pTT[2],16) &  0x6)//2
+    return(Sector,S1Board,eta,phi,CEECEH)
 
 def channel(board,link,word,args):
     Sector,S2Board = args.Sector,args.S2_Board

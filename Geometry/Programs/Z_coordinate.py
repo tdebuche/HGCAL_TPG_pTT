d4acb8749a69d9cd478068440262cda051aa29a7
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-07-02 16:59:12 +0200

    Update Z_coordinate.py

--- Geometry/Programs/Z_coordinate.py
+++ Geometry/Programs/Z_coordinate.py
@@ -7,7 +7,8 @@ ZlayerCEHfine = [3665.96,3687.51,3729.01,3750.56,3792.01,3813.61,3855.11,3876.66
 ZlayerCEHcoarse = [4378.71,4400.26,4460.96,4482.51,4543.21,4564.76,4625.46,4647.01,4707.71,4729.26,4789.96,4811.51,4872.21,4893.76,4954.46,4976.01,5036.71,5058.26,5118.96,5140.51]
 
 
-def create_Z_coordinates(L1,L2,L3):
+def create_Z_coordinates(ZlayerCEE,ZlayerCEHfine,ZlayerCEHcoarse):
+    L1,L2,L3 = ZlayerCEE,ZlayerCEHfine,ZlayerCEHcoarse
     Z = []
     layer = 1
     for i in range(len(L1)-1):
@@ -24,7 +25,5 @@ def create_Z_coordinates(L1,L2,L3):
         z = (L3[2*i]+L3[2*i+1])/2
         Z.append({'Layer' : layer, 'Z_coordinate' : z})
         layer += 1
-    return(Z)
-
-with open('src/Z_coordinates.json', 'w') as recordfile:
-    json.dump(create_Z_coordinates(ZlayerCEE,ZlayerCEHfine,ZlayerCEHcoarse), recordfile)
+    with open('src/Z_coordinates.json', 'w') as recordfile:
+        json.dump(Z, recordfile)

04f75d457dcb0e450ca314d95c95b7e22531e112
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-07-02 14:47:19 +0200

    Update XML_to_firmware_format.py

--- S1S2_Mapping/Programs/XML_to_firmware_format.py
+++ S1S2_Mapping/Programs/XML_to_firmware_format.py
@@ -1,315 +1,37 @@
+import xml.etree.ElementTree as ET
+from collections import defaultdict
+import math
 import numpy as np
-import matplotlib.pyplot as plt
-import os
-import argparse
-dir_path = os.path.dirname(os.path.realpath(__file__))
-os.chdir(dir_path+'/../Ressources')
+import awkward as ak
+from S1S2_Mapping.Programs.tools  import *
 
-parser = argparse.ArgumentParser()
-parser.add_argument("Sector", help="Layer to display",type=int)
-parser.add_argument("S2Board", help="S2 Board to record",type = int)
-parser.add_argument("Edges", default = 'yes', help="Layer to display")
-args = parser.parse_args()
-
-
-
-
-Endcap = 0
-Sector = args.Sector
-Subsector = 0
-S2 = args.S2Board
-
-def allocation4linksNoEdges(Sector,S2Board):
-    Boards = [S1ID(Sector,board) for board in range(14)]
-    text = ''
-    for i in range(len(Boards)):
-        text +=  '\t'+ '<S1 id="'+Boards[i]+'">'+'\n'
-        for j in range(4):
-            for k in range(2):
-                res = 0
-                text +=  '\t'+'\t' +'<Channel id="'+ channel(Boards[i],j,k,Sector,S2Board)+'" aux-id="'+ str(j*2+k)+'">'+'\n'
-                for eta in range(10*(k%2),10*(k%2 + 1)):
-                    for phi in range(9 * (1-j//2+1) +5, 9* (1-j//2) +5,-1):
-                        if j%2 == 0:
-                            t = tower(Boards[i],eta,phi,0,Sector)
-                        if j%2 == 1 :
-                            t = tower(Boards[i],eta,phi,1,Sector)
-                        text += '\t\t\t'+'<Frame id = "'+str(res).zfill(3)+'"  pTT="'+ t+'" />' + '\n'
-                        res +=1
-                    text += '\t\t\t'+'<Frame id = "'++str(res).zfill(3)+'" />'+'\n'
-                    res +=1
-                for f in range(res,108):
-                    text += '\t\t\t'+'<Frame id = "'+str(res).zfill(3)+'" />' + '\n'
-                    res +=1
-                text += '\t\t'+'</Channel>' + '\n'
-        text += '\t'+'</S1>'+'\n'
-
-
-
-    return text
-
-def allocation4linksEdges(Sector,S2Board):
-    Boards = [S1ID(Sector,board) for board in range(14)]
-    text = ''
-    for i in range(len(Boards)):
-        text +=  '\t'+ '<S1 id="'+Boards[i]+'">'+'\n'
-        for j in range(4):
-            for k in range(2):
-                res = 0
-                text +=  '\t'+'\t' +'<Channel id="'+ channel(Boards[i],j,k,Sector,S2Board)+'" aux-id="'+ str(j*2+k)+'">'+'\n'
-                for eta in range(10*(k%2),10*(k%2 + 1)):
-                    if j//2 != 1:
-                        if j%2 == 0:
-                            t = tower(Boards[i],eta,27,0,Sector)
-                        if j%2 ==1 :
-                            t = tower(Boards[i],eta,27,1,Sector)
-                        if res < 10:
-                            nbzeros = '00'
-                        if  res > 9:
-                            nbzeros = '0'
-                        if res > 99:
-                            nbzeros = ''
-                        text += '\t\t\t'+'<Frame id = "'+nbzeros +str( res)+'"  pTT="'+ t+'" />' + '\n'
-                        res +=1
-                    for phi in range(9 * (1-j//2+1) +8, 9* (1-j//2) +8,-1):
-                        if j%2 == 0:
-                            t = tower(Boards[i],eta,phi,0,Sector)
-                        if j%2 ==1 :
-                            t = tower(Boards[i],eta,phi,1,Sector)
-                        if res < 10:
-                            nbzeros = '00'
-                        if  res > 9:
-                            nbzeros = '0'
-                        if res > 99:
-                            nbzeros = ''
-                        text += '\t\t\t'+'<Frame id = "'+nbzeros +str( res)+'"  pTT="'+ t+'" />' + '\n'
-                        res +=1
-                    if res < 10:
-                        nbzeros = '00'
-                    if  res > 9:
-                        nbzeros = '0'
-                    if res > 99:
-                        nbzeros = ''
-                    if j//2 == 1 or res < 97:
-                        text += '\t\t\t'+'<Frame id = "'+nbzeros +str( res)+'" />'+'\n'
-                        res +=1
-                for f in range(res,108):
-                    if f < 100:
-                        nbzeros = '0'
-                    else:
-                        nbzeros = ''
-                    text += '\t\t\t'+'<Frame id = "'+nbzeros +str(f)+'" />' + '\n'
-                text += '\t\t'+'</Channel>' + '\n'
-        text += '\t'+'</S1>'+'\n'
-
-    return text
-
-def allocation2linksNoEdges(Sector,S2Board):
-    Boards = [S1ID(Sector+1,board) for board in range(14)]
-    text = ''
-    for i in range(len(Boards)):
-        text +=  '\t'+ '<S1 id="'+Boards[i]+'">'+'\n'
-        for j in range(2):
-            for k in range(2):
-                res = 0
-                text +=  '\t'+'\t' +'<Channel id="'+ channel(Boards[i],j+4,k,Sector + 1,S2Board)+'" aux-id="'+ str((j+4)*2+k)+'">'+'\n'
-                for eta in range(10*(k%2),10*(k%2 + 1)):
-                    for phi in range(5, -1,-1):
-                        if j%2 == 0:
-                            t = tower(Boards[i],eta,phi,0,Sector+1)
-                        if j%2 == 1 :
-                            t = tower(Boards[i],eta,phi,1,Sector+1)
-                        if res < 10:
-                            nbzeros = '00'
-                        if  res > 9:
-                            nbzeros = '0'
-                        text += '\t\t\t'+'<Frame id = "'+nbzeros +str( res)+'"  pTT="'+ t+'" />' + '\n'
-                        res +=1
-                    if res < 10:
-                        nbzeros = '00'
-                    if  res > 9:
-                        nbzeros = '0'
-                    if res > 99:
-                        nbzeros = ''
-                    text += '\t\t\t'+'<Frame id = "'+nbzeros +str( res)+'" />'+'\n'
-                    res +=1
-                for f in range(res,108):
-                    if f < 100:
-                        nbzeros = '0'
-                    else:
-                        nbzeros = ''
-                    text += '\t\t\t'+'<Frame id = "'+nbzeros +str(f)+'" />' + '\n'
-                text += '\t\t'+'</Channel>' + '\n'
-        text += '\t'+'</S1>'+'\n'
-
-
-
-    return text
-
-
-def allocation2linksEdges(Sector,S2Board):
-    Boards = [S1ID(Sector+1,board) for board in range(14)]
-    text = ''
-    for i in range(len(Boards)):
-        text +=  '\t'+ '<S1 id="'+Boards[i]+'">'+'\n'
-        for j in range(2):
-            for k in range(2):
-                res = 0
-                text +=  '\t'+'\t' +'<Channel id="'+ channel(Boards[i],j+4,k,Sector+1,S2Board)+'" aux-id="'+ str((j+4)*2+k)+'">'+'\n'
-                for eta in range(10*(k%2),10*(k%2 + 1)):
-                    for phi in range(8,-1,-1):
-                        if j%2 == 0:
-                            t = tower(Boards[i],eta,phi,0,Sector+1)
-                        if j%2 ==1 :
-                            t = tower(Boards[i],eta,phi,1,Sector+1)
-                        if res < 10:
-                            nbzeros = '00'
-                        if  res > 9:
-                            nbzeros = '0'
-                        if res > 99:
-                            nbzeros = ''
-                        text += '\t\t\t'+'<Frame id = "'+nbzeros +str( res)+'"  pTT="'+ t+'" />' + '\n'
-                        res +=1
-                    if res < 10:
-                        nbzeros = '00'
-                    if  res > 9:
-                        nbzeros = '0'
-                    if res > 99:
-                        nbzeros = ''
-                    if j//2 == 1 or res < 97:
-                        text += '\t\t\t'+'<Frame id = "'+nbzeros +str( res)+'" />'+'\n'
-                        res +=1
-                for f in range(res,108):
-                    if f < 100:
-                        nbzeros = '0'
-                    else:
-                        nbzeros = ''
-                    text += '\t\t\t'+'<Frame id = "'+nbzeros +str(f)+'" />' + '\n'
-                text += '\t\t'+'</Channel>' + '\n'
-        text += '\t'+'</S1>'+'\n'
-
-    return text
-
-
-
-
-
-
-def channel(board,link,word,Sector,S2Board):
-    subsystem  = 1
-    obj_type = 5
-    t = ''
-    binary = ''
-    binary += decimaltobinary(1,Endcap)
-    binary += decimaltobinary(2,Sector)
-    binary += decimaltobinary(1,Subsector)
-    binary += decimaltobinary(2,subsystem)
-    binary += decimaltobinary(4,obj_type)
-    S1_ID =  board_6ID(board)
-    binary +=  S1_ID
-    binary += decimaltobinary(5,S2Board)
-    binary += decimaltobinary(3,link)
-    binary += decimaltobinary(2,word)
-    binary += '000000'
-    return('0x'+ binarytohexa(8,binary))
-
-
-def tower(board,i,j,CEECEH,Sector):
-    subsystem  = 1
-    obj_type = 6
-    t = ''
-    binary = ''
-    binary += decimaltobinary(1,Endcap)
-    binary += decimaltobinary(2,Sector)
-    binary += decimaltobinary(1,Subsector)
-    binary += decimaltobinary(2,subsystem)
-    binary += decimaltobinary(4,obj_type)
-    S1_ID =  board_6ID(board)
-    binary +=  S1_ID
-    binary += '00000'
-    binary += str(int(CEECEH))
-    binary += decimaltobinary(5,i)   #eta
-    binary += decimaltobinary(5,j)   #phi
-    return('0x'+ binarytohexa(8,binary))
-
-def board_6ID(board):
-    doublehexa = board[4:6]
-    return(hexatobinary(8,doublehexa)[2:8])
-
-
-def S1ID(Sector,board):
-    subsystem  = 1
-    obj_type = 0
-    binary = decimaltobinary(1,Endcap)+decimaltobinary(2,Sector)+decimaltobinary(1,Subsector)
-    binary += decimaltobinary(2,subsystem)
-    binary += decimaltobinary(4,obj_type)
-    binary += decimaltobinary(6,board)
-    binary += decimaltobinary(16,0)
-    return('0x'+ binarytohexa(8,binary))
-
-
-def decimaltobinary(nbbits,number):
-    t =''
-    res = 0
-    reste = number
-    for i in range(nbbits):
-        res = reste//(2**(nbbits-1-i))
-        reste = reste - res *2**(nbbits-1-i)
-        t+= str(res)
-    return(t)
-
-def decimaltohexa(nbhexa,number):
-    t =''
-    res = 0
-    reste = number
-    hexa = ['A','B','C','D','E','F']
-    for i in range(nbhexa):
-        res = reste//(16**(nbhexa-1-i))
-        reste = reste - res *16**(nbhexa-1-i)
-        if res <10:
-            t+= str(res)
-        else:
-            t += hexa[res-10]
-    return(t)
-
-def binarytodecimal(binary):
-    t = 0
-    for i in range(len(binary)):
-            t+= int(binary[i]) * 2**(len(binary)-1-i)
-    return t
-
-
-def hexatodecimal(hexanumber):
-    t = 0
-    hexa = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
-    for i in range(len(hexanumber)):
-        for j in range(16):
-            if hexanumber[i] == hexa[j]:
-                t+= j * 16**(len(hexanumber)-1-i)
-    return t
-
-def hexatobinary(nbbits,hexanumber):
-    return(decimaltobinary(nbbits,hexatodecimal(hexanumber)))
-
-def binarytohexa(nbhexa,binary):
-    return(decimaltohexa(nbhexa,binarytodecimal(binary)))
-
-
-if args.Edges == 'yes':
-    file = open("AllocationpTTsEdges.txt", "w")
-    file.write(allocation4linksEdges(Sector,S2))
-if args.Edges == 'no':
-    file = open("AllocationpTTsNoEdges.txt", "w")
-    file.write(allocation4linksNoEdges(Sector,S2))
-file.close()
-
-if args.Edges == 'yes':
-    file = open("DuplicationpTTsEdges.txt", "w")
-    file.write(allocation2linksEdges(Sector,S2))
-if args.Edges == 'no':
-    file = open("DuplicationpTTsNoEdges.txt", "w")
-    file.write(allocation2linksNoEdges(Sector,S2))
-file.close()
-
-
+def record_firmware_mapping(args):
+    if args.Edges == 'yes':
+        path = 'S1S2_Mapping/Results/28_Phi_Bins'
+    if args.Edges == 'no':
+        path = 'S1S2_Mapping/Results/24_Phi_Bins'
+        
+    tree = ET.parse('S1S2_Mapping/Results/'++'Allocation.xml')
+    root = tree.getroot()
+    firmware_mapping = ''
+    S1_index = 0
+    for s1_element in root.findall('.//S1'):
+        for channel_element in s1_element.findall('.//Channel'):
+            channel = int(channel_element.get('aux-id'))
+            for frame_element in channel_element.findall('.//Frame'):
+                if all(attr in frame_element.attrib for attr in ['id']):
+                    frame  = frame_element.get('id')
+                    pTT  = frame_element.get('pTT')
+                    if pTT:
+                        pTT_Sector,S1Board,eta,phi,CEECEH = get_pTT_numbers(pTT)
+                        if CEECEH == 0 : CEECEH = "CE-E"
+                        if CEECEH == 1 : CEECEH = "CE-H"
+                        firmware_mapping += "Sector="+str(args.Sector)+", S2_board="+args.str(args.S2_Board)+', Frame id = '+frame+', Link='+str(channel//2)+', Word='+str(channel%2)+', pTT : S1_Board='+str(S1Board)+', eta='+str(eta)+', phi='+str(phi)+', '+ CEECEH +'\n'
+                        if S1_index != S1Board: print(error)
+                    if not pTT:
+                        firmware_mapping += "Sector="+str(args.Sector)+", S2_board="+args.str(args.S2_Board)+', Frame id = '+frame+', Link='+str(channel//2)+', Word='+str(channel%2)+', pTT : S1_Board=99, eta=99, phi=99, CE-E' +'\n'
+                    
+        S1_index += 1
+    file = open(path+"/For_Toni.txt", "w")
+    file.write(firmware_mapping)
 

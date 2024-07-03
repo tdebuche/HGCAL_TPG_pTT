f2f3a2ac56cb068b1e4a7076c9371ea5a968f6d3
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-07-03 11:54:37 +0200

    Update tools.py

--- Geometry/Programs/tools.py
+++ Geometry/Programs/tools.py
@@ -40,75 +40,12 @@ def reorganize_vertices(vertices,irot):
 
 ####################### Few function used for the conversion XY/EtaPhi  #########
 
-def pointtopolygon(L):
-    points = []
-    for i in range(len(L[0])):
-        if L[0][i]!= 0 or L[1][i] != 0:
-            points.append((L[0][i],L[1][i]))
-    return(Polygon(points))
-
-def polygontopoints(poly):
-    p = poly.exterior.coords.xy
-    L = [[],[]]
-    for i in range(len(p[0])):
-        L[0].append(p[0][i])
-        L[1].append(p[1][i])
-    return(L)
-
-def XYtoetaphi(x,y,z):
-    if np.abs(y) < 0.00001:
-        if np.sign(x)<0:
-            phi = 0
-        else :
-            phi = np.pi
-        return((-np.log(np.tan(0.5*np.arctan(np.abs(x)/(z * np.abs(np.cos(phi))))))),phi)
-    if x == 0:
-        if np.sign(y)<0:
-            phi = 3*np.pi/2
-        else:
-            phi = np.pi/2
-        return((-np.log(np.tan(0.5*np.arctan(y/(z * np.sin(phi)))))),phi)
-    elif x >0:
-        phi = np.arctan(y/x)
-        return((-np.log(np.tan(0.5*np.arctan(x/(z * np.cos(phi)))))),phi)
-    elif x < 0 :
-        phi = np.arctan(np.abs(x)/y) + np.pi/2
-        return((-np.log(np.tan(0.5*np.arctan(x/(z * np.cos(phi)))))),phi)
-
 
 def etaphitoXY(eta,phi,z):
     x = z * np.tan(2*np.arctan(np.exp(-eta))) * np.cos(phi)
     y = z * np.tan(2*np.arctan(np.exp(-eta))) * np.sin(phi)
     return(x,y)
 
-def etaphiRADtoXY(eta,phi,z):
-    x = z * np.tan(eta) * np.cos(phi)
-    y = z * np.tan(eta) * np.sin(phi)
-    return(x,y)
-
-
-def binetaphitoXY(B,z):
-    XY = np.zeros(np.shape(B))
-    for i in range(len(B)):
-        for j in range(4):
-            eta = B[i,0,j]
-            phi = B[i,1,j]
-            x,y = etaphitoXY(eta,phi,z)
-            XY[i,0,j] = x
-            XY[i,1,j] = y
-    return XY
-
-
-def binetaphiRADtoXY(B,z):
-    XY = np.zeros(np.shape(B))
-    for i in range(len(B)):
-        for j in range(4):
-            eta = B[i,0,j]
-            phi = B[i,1,j]
-            x,y = etaphiRADtoXY(eta,phi,z)
-            XY[i,0,j] = x
-            XY[i,1,j] = y
-    return XY
 
 
 

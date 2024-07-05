3aadf6e9631a2be99531d23423d23a30698120c0
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-07-05 16:46:47 +0200

    Update create_Bins.py

--- Geometry/Programs/create_Bins.py
+++ Geometry/Programs/create_Bins.py
@@ -6,7 +6,23 @@ from Geometry.Programs.tools import *
 #record two scenarios : 20*24 bins and 20*28 bins
 
 #First scenario : 20 * 24 bins, 0 to 120 degree
+def record_20_24_Bins_all_endcap():
+    bin_size = "pi/36"
+    nb_phibin = 24
+    nb_etabin = 20
+    etamin = 1.305
+    etamax = etamin + np.pi *20/36
+    phimin = 0 * np.pi/180
+    phimax = 120 * np.pi/180
+    Bins = create_Bins_all_endcap(etamin,phimin,nb_etabin,nb_phibin)
 
+
+
+
+
+
+
+    
 def record_20_24_Bins():
     bin_size = "pi/36"
     nb_phibin = 24
@@ -62,6 +78,47 @@ def create_Bins(etamin,phimin,nbeta,nbphi):
     return(all_layers_Bins)
 
 
+def create_Bins_all_endcap(etamin,phimin,nbeta,nbphi): 
+    with open('src/Z_coordinates.json','r') as file:
+        Z_Layers = json.load(file)
+    all_layers_Bins = []
+    if nb_phi = 24 : offset = 0
+    if nb_phi = 24 : offset = 3
+    for layer in range(1,48):
+        single_layer_Bins = []
+        bin_idx = 0 
+        z = Z_Layers[layer-1]['Z_coordinate']
+        for phi in range(72):
+            for eta in range(nbeta):
+                Eta_vertices,Phi_vertices = [eta * np.pi/36 + etamin,(eta+1) * np.pi/36 + etamin,(eta+1) * np.pi/36 + etamin,eta * np.pi/36 + etamin],[phi * np.pi/36 ,phi * np.pi/36,(phi+1) * np.pi/36,(phi+1) * np.pi/36 ]
+                verticesX,verticesY = [],[]
+                for vertex_idx in range(len(Eta_vertices)):
+                    x,y = etaphitoXY(Eta_vertices[vertex_idx],Phi_vertices[vertex_idx],z)
+                    verticesX.append(x)
+                    verticesY.append(y)
+                S2_sector =  ((phi-6)//24)
+                if S2_sector < 0 : S2_sector = 2
+                S2_coordinates = {'Sector': S2_sector,  'eta_index' : eta, 'phi_index' : phi%24}
+                if nbphi == 24:
+                    S1_coordinates =  {'eta_index' : eta, 'phi_index' : phi%24}
+                    nameS1coordinates  = 'S1_Sector'+str(phi//24)
+                    single_layer_Bins.append({'index' : bin_idx, 'S2_coordinates'=S2_coordinates,nameS1coordinates=S1_coordinates,'verticesX' : verticesX,'verticesY' : verticesY, 'Eta_vertices' : Eta_vertices,'Phi_vertices' : Phi_vertices})
+                if nbphi == 28 : 
+                    S1_coordinates =  {'Sector': (phi)//24,  'eta_index' : eta, 'phi_index' : phi%24 + 3}
+                    nameS1coordinates  = 'S1_Sector'+str(phi//24)
+                    if phi%24 == 0: 
+                        nameS1coordinatesbis  = 'S1_Sector'+str((phi//24 -1)%3)
+                        S1_coordinatesbis = {'eta_index' : eta, 'phi_index' : 27}
+                        single_layer_Bins.append({'index' : bin_idx, 'S2_coordinates'=S2_coordinates,nameS1coordinates=S1_coordinates,nameS1coordinatesbis=S1_coordinatesbis,'verticesX' : verticesX,'verticesY' : verticesY, 'Eta_vertices' : Eta_vertices,'Phi_vertices' : Phi_vertices})
+                    elif phi%24 > 20: 
+                        nameS1coordinatesbis  = 'S1_Sector'+str((phi//24 +1)%3)
+                        S1_coordinatesbis = {'eta_index' : eta, 'phi_index' : phi%24 -21}
+                        single_layer_Bins.append({'index' : bin_idx, 'S2_coordinates'=S2_coordinates,nameS1coordinates=S1_coordinates,nameS1coordinatesbis=S1_coordinatesbis,'verticesX' : verticesX,'verticesY' : verticesY, 'Eta_vertices' : Eta_vertices,'Phi_vertices' : Phi_vertices})
+                    else :
+                        single_layer_Bins.append({'index' : bin_idx, 'S2_coordinates'=S2_coordinates,nameS1coordinates=S1_coordinates,'verticesX' : verticesX,'verticesY' : verticesY, 'Eta_vertices' : Eta_vertices,'Phi_vertices' : Phi_vertices})
+                bin_idx +=1
+        all_layers_Bins.append(single_layer_Bins)
+    return(all_layers_Bins)
 
 
 

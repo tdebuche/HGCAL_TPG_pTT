16dcea765a96fe6cb7c3504d2ce41da47782bdca
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-07-09 12:22:19 +0200

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ from pTTs.Programs.pTTs_to_file import record_all_boards
 from S1_Input.Programs.record_text import record_input
 from Geometry.Programs.plot import plot_layer
 from Geometry.Programs.plot import record_all_layers
-from S1S2_Mapping.Programs.S2_pTT_Mapping import record_mapping
+from S1S2_Mapping.Programs.S2_pTT_Mapping import record_S2_mapping
+from S1S2_Mapping.Programs.S1_pTT_Mapping import record_S1_mapping
 from S1S2_Mapping.Programs.S2_XML_to_firmware_format import record_pTT_firmware_mapping,record_TC_firmware_mapping
 from Geometry.Programs.Z_coordinate import create_Z_coordinates
 from Geometry.Programs.create_Bins import record_20_24_Bins,record_20_28_Bins,record_20_24_Bins_all_endcap,record_20_28_Bins_all_endcap
@@ -81,7 +82,8 @@ if args.Record_plots == "yes":
 
 #S1-S2 mapping
 if args.S1S2_Mapping == "yes":
-  record_mapping(args)
+  record_S1_mapping(args)
+  record_S2_mapping(args)
   record_pTT_firmware_mapping(args)
   record_TC_firmware_mapping(args)
 
